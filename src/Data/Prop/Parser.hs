1f296144767a6ab7316091ce9110bb7ced25cce0
Artem Mavrin <artemvmavrin@gmail.com>
2021-01-23 18:24:46 -0800

    Cleaned up parser; fixed bug not allowing consecutive negations

--- src/Data/Prop/Parser.hs
+++ src/Data/Prop/Parser.hs
@@ -27,28 +27,25 @@ import Text.Parsec.Char (char, oneOf, spaces, string)
 import Text.Parsec.Combinator
   ( between,
     chainl1,
+    chainr1,
     eof,
     notFollowedBy,
     sepBy,
-    sepBy1,
   )
 import Text.Parsec.Error (ParseError)
-import Text.Parsec.Prim (ParsecT, Stream, many, parse, try, (<|>))
-import Prelude hiding (and, or)
+import Text.Parsec.Prim (ParsecT, Stream, many, parse, parserZero, try, (<|>))
+import Prelude hiding (and, or, not)
 
 -- | @(parseFormula s)@ parses a string-like value @s@ as a propositional
 -- formula, returning a @'Right' a@ on success, where @a@ is the parsed term.
 -- Otherwise, @'Left' e@ is returned, where @e@ is a 'ParseError'.
---
--- __Note:__ Parsing is currently only implemented for the implicational
--- fragment.
 --
 -- __Conventions__
 --
 -- * Supported connective symbols:
 --
 --     * Negation: @"~"@, @"¬"@.
---     * Implication: @"->"@, @"→"@, @"=>"@, @"⇒"@ @"⊃"@.
+--     * Implication: @"->"@, @"=>"@, @"→"@, @"⇾"@, @"⇒"@ @"⊃"@.
 --     * Disjunction: @"|"@, @"\\/"@, @"∨"@
 --     * Conjunction: @"&"@, @"/\\"@, @"∧"@
 --
@@ -116,32 +113,50 @@ unsafeParseFormula = either (error . show) id . parseFormula
 unsafeParseSequent :: Stream s Identity Char => s -> Sequent String
 unsafeParseSequent = either (error . show) id . parseSequent
 
+-- Top-level parsers
+
 formula :: Stream s m Char => ParsecT s u m (Formula String)
-formula = padded implication
+formula = padded implies
 
 context :: Stream s m Char => ParsecT s u m (Context String)
 context = Set.fromList <$> formula `sepBy` char ','
 
-conclusion :: Stream s m Char => ParsecT s u m (Formula String)
-conclusion = turnstile *> formula
-
 sequent :: Stream s m Char => ParsecT s u m (Sequent String)
-sequent = (,) <$> context <*> conclusion
+sequent = (,) <$> context <*> (spaces *> turnstile *> formula)
 
-implication :: Stream s m Char => ParsecT s u m (Formula String)
-implication = foldr1 Imp <$> junction `sepBy1` try rightArrow
+-- Connective symbols
 
--- Conjunctions and disjunctions, read left-to-right
-junction :: Stream s m Char => ParsecT s u m (Formula String)
-junction = chainl1 negation $ try $ padded $ and <|> or
-  where
-    and = And <$ wedge
-    or = Or <$ vee
+implication :: Stream s m Char => ParsecT s u m String
+implication = connective ["->", "=>", "→", "⇾", "⇒", "⊃"]
 
-negation :: Stream s m Char => ParsecT s u m (Formula String)
-negation = try (neg *> (Not <$> term)) <|> term
-  where
-    term = enclosed formula <|> variable
+and :: Stream s m Char => ParsecT s u m String
+and = connective ["&", "∧", "/\\"]
+
+or :: Stream s m Char => ParsecT s u m String
+or = connective ["|", "∨", "\\/"]
+
+negation :: Stream s m Char => ParsecT s u m String
+negation = connective ["~", "¬"]
+
+turnstile :: Stream s m Char => ParsecT s u m String
+turnstile = connective ["|-", "⊢"]
+
+-- Propositional formula components
+
+-- Right-associative nested implications
+implies :: Stream s m Char => ParsecT s u m (Formula String)
+implies = chainr1 andOr (try $ padded (Imp <$ implication))
+
+-- Left-associative nested disjunctions and conjunctions
+andOr :: Stream s m Char => ParsecT s u m (Formula String)
+andOr = chainl1 not (try $ padded $ (And <$ and) <|> (Or <$ or))
+
+-- Zero or more consecutive negations
+not :: Stream s m Char => ParsecT s u m (Formula String)
+not = spaces *> (foldl (.) id <$> try (many (Not <$ negation)) <*> atom)
+
+atom :: Stream s m Char => ParsecT s u m (Formula String)
+atom = enclosed formula <|> variable
 
 variable :: Stream s m Char => ParsecT s u m (Formula String)
 variable = Var <$> name
@@ -149,6 +164,8 @@ variable = Var <$> name
     name = (:) <$> nameStart <*> many nameChar
     nameStart = oneOf $ '_' : ['a' .. 'z'] ++ ['A' .. 'Z']
     nameChar = nameStart <|> oneOf ('\'' : ['0' .. '9'])
+
+-- Helper functions
 
 -- Ignore space on either side of a parsed string.
 padded :: Stream s m Char => ParsecT s u m a -> ParsecT s u m a
@@ -158,35 +175,12 @@ padded = between spaces spaces
 enclosed :: Stream s m Char => ParsecT s u m a -> ParsecT s u m a
 enclosed = between (char '(') (char ')')
 
--- Parse an implication arrow.
-rightArrow :: Stream s m Char => ParsecT s u m String
-rightArrow =
-  padded $
-    try (string "->")
-      <|> string "→"
-      <|> try (string "=>")
-      <|> string "⇒"
-      <|> string "⊃"
-
--- Parse a conjunction (and) symbol.
-wedge :: Stream s m Char => ParsecT s u m String
-wedge =
-  padded $
-    string "&"
-      <|> string "∧"
-      <|> try (string "/\\")
-
--- Parse a disjunction (or) symbol.
-vee :: Stream s m Char => ParsecT s u m String
-vee =
-  padded $
-    try (string "|" <* notFollowedBy (char '-')) -- needed to handle "|-"
-      <|> string "∨"
-      <|> try (string "\\/")
-
-neg :: Stream s m Char => ParsecT s u m String
-neg = spaces *> (string "~" <|> string "¬")
-
--- Parse a sequent/judgement turnstile.
-turnstile :: Stream s m Char => ParsecT s u m String
-turnstile = padded (string "|-" <|> string "⊢")
+-- Generate a parser out of a collection of strings. The parser will parse any
+-- one of the given strings.
+connective :: Stream s m Char => [String] -> ParsecT s u m String
+connective [] = parserZero
+connective (c : cs)
+  -- Special case needed to handle the turnstile symbol "|-" separately from the
+  -- disjunction symbol "|".
+  | c == "|" = try (string "|" <* notFollowedBy (char '-')) <|> connective cs
+  | otherwise = try (string c) <|> connective cs
