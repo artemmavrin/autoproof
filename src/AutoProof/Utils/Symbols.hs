89d67d0e2e7a090f4f8c8e4317cb554d32882780
Artem Mavrin <artemvmavrin@gmail.com>
2021-03-02 19:26:20 -0800

    Added remaining inference rule symbols

--- src/AutoProof/Utils/Symbols.hs
+++ src/AutoProof/Utils/Symbols.hs
@@ -1,7 +1,43 @@
 {-# LANGUAGE CPP #-}
 {-# OPTIONS_HADDOCK hide, prune #-}
 
-module AutoProof.Utils.Symbols where
+module AutoProof.Utils.Symbols
+  ( -- * Formula symbols
+    falseS,
+    trueS,
+    notS,
+    impS,
+    orS,
+    andS,
+    iffS,
+
+    -- * Judgement symbols
+    turnstileS,
+
+    -- * Proof symbols
+    axiomS,
+    falseElimS,
+    trueIntrS,
+    notElimS,
+    notIntrS,
+    impElimS,
+    impIntrS,
+    orElimS,
+    orIntrLS,
+    orIntrRS,
+    andElimLS,
+    andElimRS,
+    andIntr,
+    iffElimLS,
+    iffElimRS,
+    iffIntrS,
+
+    -- * Proof pretty-printing
+    vertS,
+    cornerS,
+    branchS,
+  )
+where
 
 -- Propositional literals, connectives, and judgement turnstile
 
@@ -52,11 +88,52 @@ impElimS = elim impS
 impIntrS :: String
 impIntrS = intr impS
 
+orElimS :: String
+orElimS = elim orS
+
+orIntrLS :: String
+orIntrLS = intrL orS
+
+orIntrRS :: String
+orIntrRS = intrR orS
+
+andElimLS :: String
+andElimLS = elimL andS
+
+andElimRS :: String
+andElimRS = elimR andS
+
+andIntr :: String
+andIntr = intr andS
+
+iffElimLS :: String
+iffElimLS = elimL iffS
+
+iffElimRS :: String
+iffElimRS = elimR iffS
+
+iffIntrS :: String
+iffIntrS = intr iffS
+
+-- Inference rule symbol helper functions
+
 elim :: String -> String
 elim s = "(" ++ s ++ "E)"
 
+elimL :: String -> String
+elimL s = "(" ++ s ++ "EL)"
+
+elimR :: String -> String
+elimR s = "(" ++ s ++ "ER)"
+
 intr :: String -> String
 intr s = "(" ++ s ++ "I)"
+
+intrL :: String -> String
+intrL s = "(" ++ s ++ "IL)"
+
+intrR :: String -> String
+intrR s = "(" ++ s ++ "IR)"
 
 -- Pretty-printed proof components
 
